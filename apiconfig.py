dc36b898fd18bb2ae54b4a235fa5e67b9dd9fd61
katyshawn922 <45888847+katyshawn922@users.noreply.github.com>
2021-05-03 00:30:17 +0800

    Update apiconfig.py

--- apiconfig.py
+++ apiconfig.py
@@ -2,19 +2,19 @@
 NODE_ID = 0
 
 # hour,set 0 to disable
-SPEEDTEST = 6
-CLOUDSAFE = 1
+SPEEDTEST = 0
+CLOUDSAFE = 0
 ANTISSATTACK = 0
 AUTOEXEC = 0
 
-MU_SUFFIX = "zhaoj.in"
-MU_REGEX = "%5m%id.%suffix"
+MU_SUFFIX = ""
+MU_REGEX = "%suffix"
 
 SERVER_PUB_ADDR = "127.0.0.1"  # mujson_mgr need this to generate ssr link
 API_INTERFACE = "modwebapi"  # glzjinmod, modwebapi
 
-WEBAPI_URL = "https://zhaoj.in"
-WEBAPI_TOKEN = "glzjin"
+WEBAPI_URL = "https://"
+WEBAPI_TOKEN = ""
 
 # mudb
 MUDB_FILE = "mudb.json"
